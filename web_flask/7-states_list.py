9410f177d4e885291483d19121d263618a1fa8c9
GillesR76 <gilles-richard76@hotmail.com>
2024-04-10 09:40:26 +0200

    script that starts a Flask web application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, appcontext_tearing_down
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list/', strict_slashes=False)
+def state_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
