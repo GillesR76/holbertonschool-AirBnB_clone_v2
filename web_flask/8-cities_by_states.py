d60c02e030f7dee04acfc0ce5da753c7454308ae
GillesR76 <gilles-richard76@hotmail.com>
2024-04-10 13:35:43 +0200

    add a docstring and pycodestyle correction

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,11 +11,16 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states/', strict_slashes=False)
 def cities_by_state():
+    """
+    Route decorator to display cities by states with
+    sorted states and their cities
+    """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     sorted_cities = {state: sorted(
         state.cities, key=lambda city: city.name) for state in sorted_states}
-    return render_template('8-cities_by_states.html', states=sorted_states, cities=sorted_cities)
+    return render_template('8-cities_by_states.html',
+                           states=sorted_states, cities=sorted_cities)
 
 
 @app.teardown_appcontext
