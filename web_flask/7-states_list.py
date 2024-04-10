a39ceb4d6b65bb45310c8389a5bcb7caf17fcaae
GillesR76 <gilles-richard76@hotmail.com>
2024-04-10 10:38:56 +0200

    Add shebang and docstring to Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Flask web application to display the HBNB data"""
+
 from flask import Flask, render_template, appcontext_tearing_down
 from markupsafe import escape
 from models import storage
