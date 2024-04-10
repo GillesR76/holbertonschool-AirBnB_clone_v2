635e471e331fd40829d74bbb0f92daeed41daa34
GillesR76 <gilles-richard76@hotmail.com>
2024-04-10 10:30:22 +0200

    Refactor hello route and remove unnecessary comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,16 +9,6 @@ look for resources like templates and static files
 
 app = Flask(__name__)
 
-"""
-* defines a route for the root URL '/'. @app.route is
-a decorator used to associate the index function with
-the root URL
-* The strict_slashes=False option means that URLs with or
-without a trailing slash will be matched.
-* The index function returns a string which will be
-displayed in the browser when the root URL is accessed.
-"""
-
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -26,15 +16,5 @@ def index():
     return 'Hello HBNB!'
 
 
-"""
-* checks if the script is being run directly (not imported as a module).
-If it is the main script, it calls the app.run method to start the Flask
-development server.
-* The app.run() function is used to start the Flask development server
-and run the Flask application. It is typically called at the end of the
-script, after defining all the routes and configurations.
-* The host and port optional arguments allow to specify the host and
-port on which the Flask application should listen for incoming requests.
-"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
