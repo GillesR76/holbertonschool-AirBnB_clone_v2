f11d33b431dab6edc9db2188a54cbc47bafd908c
GillesR76 <gilles-richard76@hotmail.com>
2024-04-11 10:55:41 +0200

    Refactor state_cities function and update template to display cities for a specific state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,15 +14,15 @@ app = Flask(__name__)
 def state_cities(id=None):
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-    if id is None:
-        return render_template('9-states.html', states=sorted_states)
-    else:
+    if id is not None:
         for state in sorted_states:
             if id == state.id:
                 sorted_cities = {state: sorted(
                     state.cities, key=lambda city: city.name)}
-                return render_template('9-states.html', states=sorted_states, cities=sorted_cities)
-        return render_template('9-states.html', states=None, message='Not found!')
+                return render_template('9-states.html', state=state, cities=sorted_cities)
+        return render_template('9-states.html', message='Not found!')
+    else:
+        return render_template('9-states.html', states=sorted_states)
 
 
 @app.teardown_appcontext
